70780a8fcf5cb119d30c49decfc2f383caf8ef9b
Wim Vander Schelden <ks3359899.kimsufi.com>
2013-08-28 12:32:17 +0200

    Split CSV generation and database population so they can be run at different times.

--- run.rb
+++ run.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
+def run_command(command) 
+  if command == 'fetch'
+    puts "Fetching new data"
+    load "hetzner-robot.rb"
+  elsif command == 'csv'
+    puts "Generating new CSV data"
+    load "csvgen.rb"
+  else
+    puts "Unknown command: #{command}"
+  end
+end
+
 dir = File.dirname(__FILE__)
+
+commands = ARGV.map { |a| a.downcase }
 
 origdir = Dir.pwd
 Dir.chdir(dir)
-load "hetzner-robot.rb"
-load "csvgen.rb"
+commands.each { |c| run_command(c) }
 File.open('hetzner-robot.log', 'a') { |f| f.puts "Ran at #{Time.now}" }
 Dir.chdir(origdir)
